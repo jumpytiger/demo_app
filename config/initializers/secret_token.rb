e42c99dc5c33166abc6c655c35724038c9c96b84
tech <tech@jumpytigerinc.com>
2014-02-12 12:14:09 -0800

    Initial commit for demo_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '38f0d9a34a1e32b3d1c2cc53767e9a188d2d336ad9afe1f030f7f26abd62e2321c2f0072c316fd47791c7b586b8e6378d937ec96bf612a7af774030819e9e9db'
